099eda156cf45773d6f63e88e22bca1b567f2965
Toshit Chawda <r58playz@gmail.com>
2026-05-21 10:14:39 -0700

    fsolve solver

--- src/python/solver.py
+++ src/python/solver.py
@@ -1,19 +1,41 @@
-from sympy import pi, solveset, nsolve, Eq, Interval, FiniteSet, N
+from sympy import pi, solveset, nsolve, Eq, Interval, FiniteSet, N, lambdify
 from sympy.abc import theta
+from scipy.optimize import fsolve
+import numpy as np
 
 from time import monotonic
 
-STRATEGY = "solveset"
+STRATEGY = "fsolve"
+PRECISION = 9
 
 def solveset_polar(exprs, domain, solutions):
     eqn = Eq(exprs[0], exprs[1])
 
-    standard = solveset(eqn, theta, domain=domain)
-    if isinstance(standard, FiniteSet):
-        for theta_sym in standard:
-            theta_float = N(theta_sym, 5)
-            r = exprs[0].evalf(subs={theta: theta_sym}, n=5)
+    solves = solveset(eqn, theta, domain=domain)
+    if isinstance(solves, FiniteSet):
+        for theta_sym in solves:
+            theta_float = N(theta_sym, PRECISION)
+            r = exprs[0].evalf(subs={theta: theta_sym}, n=PRECISION)
             solutions.append({ "solution": "standard", "theta": theta_float, "r": r })
+
+def fsolve_polar(exprs, domain, solutions):
+    eqn = lambdify(theta, (exprs[0]) - (exprs[1]), modules=["numpy"])
+
+    if not isinstance(domain, Interval):
+        raise Exception("invalid domain");
+
+    d_start = float(domain.start)
+    d_end = float(domain.end)
+    guesses = np.linspace(d_start, d_end, num=1000, dtype=float)
+    solves = fsolve(eqn, guesses);
+
+    solves = solves[(solves >= d_start) & (solves <= d_end)];
+    solves = np.unique(np.round(solves, decimals=PRECISION));
+
+    for theta_nf in solves:
+        theta_f = float(theta_nf)
+        r = exprs[0].evalf(subs={theta: theta_f}, n=PRECISION)
+        solutions.append({ "solution": "standard", "theta": theta_f, "r": r })
 
 def solve_polar_intersections(exprs):
     domain = Interval(0, 2*pi)
@@ -22,6 +44,8 @@ def solve_polar_intersections(exprs):
     solve_before = monotonic();
     if STRATEGY == "solveset":
         solveset_polar(exprs, domain, solutions);
+    elif STRATEGY == "fsolve":
+        fsolve_polar(exprs, domain, solutions);
     else:
         raise Exception("invalid strategy");
     solve_after = monotonic();
